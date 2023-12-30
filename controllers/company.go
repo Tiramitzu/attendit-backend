acda617cfd6100a8a28cab930bb89b5c392cc61f
Tiramitzu <github@tira.my.id>
2023-12-30 13:32:42 +0700

    chore: cache company members

--- controllers/company.go
+++ controllers/company.go
@@ -40,52 +40,38 @@ func GetCompany(c *gin.Context) {
 	response.SendResponse(c)
 }
 
+func GetCompanyMembers(c *gin.Context) {
 	response := &models.Response{
 		StatusCode: http.StatusBadRequest,
 		Success:    false,
 	}
 
+	page, _ := strconv.Atoi(c.Param("page"))
+	if page == 0 {
+		page = 1
 	}
 
+	users, err := redisServices.GetCompanyMembersFromCache(page)
 	if err == nil {
-		models.SendResponseData(c, gin.H{"company": company, "cache": true})
+		models.SendResponseData(c, gin.H{"users": users, "cache": true})
 		return
 	}
 
+	users, err = services.GetCompanyMembers(page)
 	if err != nil {
 		response.Message = err.Error()
 		response.SendErrorResponse(c)
 		return
 	}
 
+	redisServices.CacheCompanyMembers(users, page)
 
 	response.StatusCode = http.StatusOK
 	response.Success = true
+	response.Data = gin.H{"users": users}
 	response.SendResponse(c)
 }
 
-func GetCompanyMembers(c *gin.Context) {
-	companyIdHex := c.Param("id")
-	page, _ := strconv.Atoi(c.Param("page"))
-	if page == 0 {
-		page = 1
-	}
-	companyId, err := primitive.ObjectIDFromHex(companyIdHex)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"message": strconv.Itoa(http.StatusBadRequest) + ": Invalid ID"})
-		return
-	}
-
-	company, err := services.GetCompanyById(companyId)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"message": strconv.Itoa(http.StatusBadRequest) + ": Invalid ID"})
-		return
-	}
-
-	users := services.FindMembersByCompanyId(company.ID, page)
-
-	c.JSON(http.StatusOK, users)
-}
 func ModifyCompany(c *gin.Context) {
 	_ = c.ShouldBindJSON(&company)
 

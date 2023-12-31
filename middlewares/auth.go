609be569673dde33f4119022721b5752b4b1e7e5
Tiramitzu <github@tira.my.id>
2024-01-01 03:04:51 +0700

    chore: ip whitelisting

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -1,7 +1,10 @@
 package middlewares
 
 import (
+	"errors"
+	"net"
 	"net/http"
+	"strings"
 
 	"attendit/backend/services"
 
@@ -10,6 +13,25 @@ import (
 
 func JWTMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		ipAddress, err := getClientIP(c.Request)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
+			return
+		}
+
+		company, err := services.GetCompany()
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
+			return
+		}
+
+		for i := 0; i < len(company.IPAddresses); i++ {
+			if ipAddress == company.IPAddresses[i] {
+				c.Next()
+				return
+			}
+		}
+
 		token := c.GetHeader("Authorization")
 		tokenModel, err := services.VerifyToken(token[7:])
 		if err != nil {
@@ -22,4 +44,32 @@ func JWTMiddleware() gin.HandlerFunc {
 
 		c.Next()
 	}
+}
+
+func getClientIP(r *http.Request) (string, error) {
+	ips := r.Header.Get("X-Forwarded-For")
+	splitIps := strings.Split(ips, ",")
+
+	if len(splitIps) > 0 {
+		netIP := net.ParseIP(splitIps[len(splitIps)-1])
+		if netIP != nil {
+			return netIP.String(), nil
+		}
+	}
+
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		return "", err
+	}
+
+	netIP := net.ParseIP(ip)
+	if netIP != nil {
+		ip := netIP.String()
+		if ip == "::1" {
+			return "127.0.0.1", nil
+		}
+		return ip, nil
+	}
+
+	return "", errors.New("IP not found")
 }

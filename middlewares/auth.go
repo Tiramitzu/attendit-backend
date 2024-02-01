e4bcc1b000b1a1a0c6fadf4c149a752e37b8b8b9
Tiramitzu <github@tira.my.id>
2024-02-01 17:23:50 +0700

    chore(middlewares/auth): added IsAdmin functionality

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -32,3 +32,46 @@ func JWTMiddleware() gin.HandlerFunc {
 		c.Next()
 	}
 }
+
+func IsAdminMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token := c.GetHeader("Authorization")
+		if len(token) < 7 {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"success": false,
+				"message": "Invalid token",
+			})
+			return
+		}
+		tokenModel, err := services.VerifyToken(token[7:])
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"success": false,
+				"message": err.Error(),
+			})
+			return
+		}
+
+		user, err := services.GetUserById(tokenModel.User)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"success": false,
+				"message": err.Error(),
+			})
+			return
+		}
+
+		if user.AccessLevel != 1 {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"success": false,
+				"message": "Unauthorized",
+			})
+			return
+		}
+
+		c.Set("userIdHex", tokenModel.User.Hex())
+		c.Set("userId", tokenModel.User)
+
+		c.Next()
+	}
+}

a2c6742e87400e7292876641318e3a6f5e66b9d5
Tiramitzu <github@tira.my.id>
2024-02-02 19:02:10 +0700

    chore(services/admin): remove verification

--- services/admin.service.go
+++ services/admin.service.go
@@ -4,7 +4,6 @@ import (
 	db "attendit/backend/models/db"
 	"github.com/kamva/mgm/v3"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
@@ -17,40 +16,6 @@ func GetUsers(page int) ([]*db.User, error) {
 
 	if err != nil {
 		return nil, err
-	}
-
-	return users, nil
-}
-
-func VerifyUser(userId primitive.ObjectID) (*db.User, error) {
-	user, err := GetUserById(userId)
-	if err != nil {
-		return nil, err
-	}
-
-	user.IsVerified = true
-	err = mgm.Coll(&db.User{}).Update(user)
-
-	if err != nil {
-		return user, err
-	}
-
-	return user, nil
-}
-
-func DeleteUnVerifiedUsers() ([]*db.User, error) {
-	var users []*db.User
-	err := mgm.Coll(&db.User{}).SimpleFind(&users, bson.M{"isVerified": false})
-
-	if err != nil {
-		return nil, err
-	}
-
-	for _, user := range users {
-		err = mgm.Coll(&db.User{}).Delete(user)
-		if err != nil {
-			return nil, err
-		}
 	}
 
 	return users, nil

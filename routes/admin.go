abab373ff2e0d8c1beda7428e080a23cd0ceef3f
Tiramitzu <github@tira.my.id>
2024-02-04 13:20:58 +0700

    chore(routes/admin): consistency

--- routes/admin.go
+++ routes/admin.go
@@ -2,7 +2,6 @@ package routes
 
 import (
 	"attendit/backend/controllers"
-	"attendit/backend/middlewares"
 	"attendit/backend/middlewares/validators"
 	"github.com/gin-gonic/gin"
 )
@@ -10,33 +9,33 @@ import (
 func AdminRoute(router *gin.RouterGroup, handlers ...gin.HandlerFunc) {
 	admin := router.Group("/admin", handlers...)
 	{
-		admin.PUT("/user", controllers.CreateUser)
+		admin.PUT("/users", controllers.CreateUser)
+		admin.GET("/users",
+			validators.QueryPageValidator(),
+			controllers.GetUsers)
+		admin.GET(
+			"/users/:id",
+			validators.PathIdValidator(),
+			controllers.GetUser,
+		)
 		admin.GET(
 			"/attendances",
 			validators.QueryPageValidator(),
 			controllers.GetAttendances,
 		)
 		admin.GET(
-			"/user/:id",
-			validators.PathIdValidator(),
-			controllers.GetUser,
-		)
-		admin.GET(
-			"/user/:id/attendances",
+			"/users/:id/attendances",
+			validators.QueryPageValidator(),
 			validators.PathIdValidator(),
 			controllers.GetUserAttendances,
 		)
-		admin.GET("/users",
-			validators.QueryPageValidator(),
-			controllers.GetUsers)
 		admin.PATCH(
-			"/user/:id",
+			"/users/:id",
 			validators.PathIdValidator(),
 			controllers.UpdateUser,
 		)
 		admin.PATCH(
 			"/company",
-			middlewares.IsAdminMiddleware(),
 			controllers.ModifyCompany,
 		)
 	}
